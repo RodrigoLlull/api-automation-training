6abe1fde93622ae2fd55fe318d95bac264571367
Rodrigo Llull <Rodrigo.Llull@endava.com>
2025-05-20 13:52:37 -0300

    add explicit Promise<Response<T>> return types to CatService methods

--- framework/src/models/service/CatService.ts
+++ framework/src/models/service/CatService.ts
@@ -1,32 +1,33 @@
 import { ServiceBase } from "../../base/ServiceBase";
 import { Cat } from "../responses/CatResponse";
+import { Response } from "../responses/Response";
 
 export class CatService extends ServiceBase {
   constructor() {
     super("/cats");
   }
 
-  async getAllCats() {
+  async getAllCats(): Promise<Response<Cat[]>> {
     return await this.get<Cat[]>(this.url);
   }
 
-  async createCat(cat: Cat) {
+  async createCat(cat: Cat): Promise<Response<Cat>> {
     return await this.post<Cat>(this.url, cat);
   }
 
-  async getCatById(catId: number) {
+  async getCatById(catId: number): Promise<Response<Cat>> {
     return await this.get<Cat>(`${this.url}/${catId}`);
   }
 
-  async deleteCat(catId: number) {
+  async deleteCat(catId: number): Promise<Response<Cat>> {
     return await this.delete(`${this.url}/${catId}`);
   }
 
-  async patchCat(catId: number, data: Partial<Cat>) {
+  async patchCat(catId: number, data: Partial<Cat>): Promise<Response<Cat>> {
     return await this.patch<Cat>(`${this.url}/${catId}`, data);
   }
 
-  async updateCat(catId: number, cat: Cat) {
+  async updateCat(catId: number, cat: Cat): Promise<Response<Cat>> {
     return await this.put<Cat>(`${this.url}/${catId}`, cat);
   }
 }
