8c68b3a25ca128266be6d00b302becfec01b495e
Rodrigo Llull <Rodrigo.Llull@endava.com>
2025-05-21 13:20:17 -0300

    Add CI pipeline to satrt API and run tests

---
+++ framework/src/tests/Dummy.spec.ts
@@ -0,0 +1,8 @@
+import "chai/register-should.js";
+
+describe.skip("Pipeline initial tests", () => {
+  it("Dummy success test", () => {
+    var result = true;
+    result.should.be.true;
+  });
+});
